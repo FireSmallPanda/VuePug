ff3172221f7af0fd869ca3cfbb440035f56369ee
weihao_ling <1020529941@qq.com>
2018-12-09 16:40:59 +0800

    fix:添加文件上传页面

--- bk-web/src/router/index.js
+++ bk-web/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Login from '@/views/Login'
 import Base from '@/views/base/Base'
+import FilesPage from '@/views/base/FilesPage'
 Vue.use(Router)
 
 const routesItems = [
@@ -14,6 +15,11 @@ const routesItems = [
     path: '/base',
     name: 'Base',
     component: Base
+  },
+  {
+    path: '/filespage',
+    name: 'FilesPage',
+    component: FilesPage
   },
   {
     path: '/',
---
+++ bk-web/src/views/base/FilesPage.vue
@@ -0,0 +1,38 @@
+<template lang='pug'>
+  .base 
+    el-upload.upload-demo(:action="actionApi+'/file'"  multiple :limit="1" :file-list='fileList' name='file' :data="fileObj" :on-success="uploadSuccess")
+        el-button(size='small', type='primary') 点击上传
+        .el-upload__tip(slot='tip') 只能上传jpg/png文件，且不超过500kb
+    img(:src="actionApi+'/file?path='+picUrl" alt='图片')
+</template>
+
+<script>
+const ACTION_API = 'http://localhost:3000/files-api'
+export default {
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      actionApi:ACTION_API,
+      fileList:[], // 文件列表
+      fileObj:{ // 额外参数
+          document:'asd'
+      },
+      picUrl:''
+
+    }
+  },
+  methods: {
+    // 上传成功后调用
+    uploadSuccess(res){
+        console.log(res)
+        this.picUrl = res.url
+    }
+  }
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
